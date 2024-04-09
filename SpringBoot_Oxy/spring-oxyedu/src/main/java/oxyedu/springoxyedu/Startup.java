0e24d07fec981703bd5d3ca1d1edaaf14b682487
Rafaelribeirodzz <rafaelribeirodzz@gmail.com>
2024-04-09 11:55:59 -0300

    segundo

--- SpringBoot_Oxy/spring-oxyedu/src/main/java/oxyedu/springoxyedu/Startup.java
+++ SpringBoot_Oxy/spring-oxyedu/src/main/java/oxyedu/springoxyedu/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringOxyeduApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringOxyeduApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
