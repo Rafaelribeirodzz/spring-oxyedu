5e5980fbd3017bcbac8a1a2c7b1aeafe1c41be93
Rafaelribeirodzz <rafaelribeirodzz@gmail.com>
2024-04-09 11:40:41 -0300

    primeiro

---
+++ SpringBoot_Oxy/spring-oxyedu/src/main/java/oxyedu/springoxyedu/Startup.java
@@ -0,0 +1,13 @@
+package oxyedu.springoxyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOxyeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOxyeduApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Oxy/spring-oxyedu/src/test/java/oxyedu/springoxyedu/SpringOxyeduApplicationTests.java
@@ -0,0 +1,13 @@
+package oxyedu.springoxyedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOxyeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
